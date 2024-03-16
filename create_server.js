e1b5f327a74b7d02c9f1cb3936050355ba91042b
Jayesh Chauhan <jc814250@gmail.com>
2024-03-16 11:04:16 +0530

    create server

--- create_server.js
+++ create_server.js
@@ -23,25 +23,35 @@ server.on('request', (req, res) => {
     // });
     let params = req.url.split('/');
     if (params[1] === 'freinds') {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'application/json');
-        if (params.length == 3) {
-            let seacrhIndex = Number(params[2]);
-            if (friends[seacrhIndex] != undefined) {
-                res.end(JSON.stringify(friends[seacrhIndex]));
+        if (req.method === 'GET') {
+
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'application/json');
+            if (params.length == 3) {
+                let seacrhIndex = Number(params[2]);
+                if (friends[seacrhIndex] != undefined) {
+                    res.end(JSON.stringify(friends[seacrhIndex]));
+                } else {
+                    res.statusCode = 404;
+                    res.end();
+                }
             } else {
-                res.statusCode = 404;
-                res.end();
+                res.end(JSON.stringify(friends));
             }
-        } else {
-            res.end(JSON.stringify(friends));
+        } else if (req.method === 'POST') {
+            req.on('data', (data) => {
+                let friend = data.toString();
+                console.log('Request:', friend);
+                friends.push(JSON.parse(friend));
+            });
+            req.pipe(res);
         }
-    } else if (req.url === '/html') {
+    } else if (params[1] === 'html') {
         res.setHeader('Content-type', 'text/html');
         const html = '<h1>This is HTML</h1>';
         res.write(html);
         res.end();
-    } else if (req.url === '/text') {
+    } else if (params[1] === 'text') {
         res.setHeader('Content-type', 'text/plain');
         const text = 'This is text';
         res.write(text);
